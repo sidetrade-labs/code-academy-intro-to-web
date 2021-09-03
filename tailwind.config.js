96f139f145828f5735d07279ad733d4a2fcf141e
Kieran <61139952+kraine93@users.noreply.github.com>
2021-09-03 11:38:02 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        "45/100": "45%",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
